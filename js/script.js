88e5f287bd16dc9f696d24158b7b8fa47de08e37
Giosuè Santoro <giosuesantoro@hotmail.it>
2021-01-26 15:24:47 +0100

    completo

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var app = new Vue ({
+    el: '#root',
+    data: {
+        messaggio: '',
+        immagine: 'https://cdn.icon-icons.com/icons2/2415/PNG/512/vuejs_original_wordmark_logo_icon_146305.png',
+    }
+})
+
+Vue.config.devtools = true;
